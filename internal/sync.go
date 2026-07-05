8886f8a8825a0a4aefa9500201ed212a31da9b58
Benjamin <7@toru.ca>
2026-07-05 07:00:26 -0400

    fix: dynamic id

--- internal/sync.go
+++ internal/sync.go
@@ -88,7 +88,7 @@ func syncWidget(appID, token, discordID, username string, aeInfo WikiInfo, ae Us
 		return err
 	}
 
-	url := fmt.Sprintf("https://discord.com/api/v9/applications/%s/users/%s/identities/0/profile", appID, discordID)
+	url := fmt.Sprintf("https://discord.com/api/v9/applications/%s/users/%s/identities/%s/profile", appID, discordID, discordID)
 	req, err := http.NewRequest(http.MethodPatch, url, bytes.NewReader(body))
 	if err != nil {
 		return err
